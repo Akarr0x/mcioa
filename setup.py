672606361c0358c0062f0d049b36fa44f2a67ed3
Akarr0x <alessandrodiamanti1@gmail.com>
2024-11-23 01:08:07 +0100

    allora

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mcioa',
+    name='mcoia',
     version='0.9.0',
     author='Alessandro Diamanti',
     author_email='alessandrodiamanti1@gmail.com',
